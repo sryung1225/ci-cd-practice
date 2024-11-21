34e47b82af84d896640ea7c45b1d7d08156aa7a6
sryung <sryung1225@gmail.com>
2024-11-21 10:22:06 +0900

    타입 설치 + 기본 유닛테스트 적용

---
+++ src/app/page.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "./page";
+
+describe("Page", () => {
+  it("renders the page", () => {
+    render(<Home />);
+    expect(
+      screen.getByText((content) =>
+        content.startsWith("Get started by editing")
+      )
+    ).toBeInTheDocument();
+  });
+});
